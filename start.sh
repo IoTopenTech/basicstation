e64819f27690901bbb9f4153ea21a6ce44cbaf3b
mpous <marc@balena.io>
2020-07-02 20:37:08 +0200

    EUI as a tag on the dashboard

--- start.sh
+++ start.sh
@@ -1,18 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
-
-# declare map of hardware pins to GPIO on Raspberry Pi
-declare -a pinToGPIO
-pinToGPIO=( -1 -1 -1 2 -1 3 -1 4 14 -1 15 17 18 27 -1 22 23 -1 24 10 -1 9 25 11 8 -1 7 0 1 5 -1 6 12 13 -1 19 16 26 20 -1 21)
-
-
-cd examples/live-s2.sm.tc
-
-# Default to TTN server
-TC_URI=${TC_URI:-"wss://lns.eu.thethings.network:443"} 
-TC_TRUST=${TC_TRUST:-$(curl https://letsencrypt.org/certs/trustid-x3-root.pem.txt)}
-
-# Introduce the EUI from the mac address as a balenaCloud TAG
 TAG_KEY="EUI"
 TTN_EUI=$(cat /sys/class/net/eth0/address | sed -r 's/[:]+//g' | sed -e 's#\(.\{6\}\)\(.*\)#\1FFFE\2#g')
 
@@ -28,6 +15,18 @@ TAG=$(curl -sX POST \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $BALENA_API_KEY" \
 --data "{ \"device\": \"$ID\", \"tag_key\": \"$TAG_KEY\", \"value\": \"$TTN_EUI\" }" > /dev/null)
+
+
+# declare map of hardware pins to GPIO on Raspberry Pi
+declare -a pinToGPIO
+pinToGPIO=( -1 -1 -1 2 -1 3 -1 4 14 -1 15 17 18 27 -1 22 23 -1 24 10 -1 9 25 11 8 -1 7 0 1 5 -1 6 12 13 -1 19 16 26 20 -1 21)
+
+
+cd examples/live-s2.sm.tc
+
+# Default to TTN server
+TC_URI=${TC_URI:-"wss://lns.eu.thethings.network:443"} 
+TC_TRUST=${TC_TRUST:-$(curl https://letsencrypt.org/certs/trustid-x3-root.pem.txt)}
 
 
 GW_RESET_PIN=${GW_RESET_PIN:-11}
